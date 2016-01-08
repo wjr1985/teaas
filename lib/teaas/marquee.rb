4fe9522d42bfc384a8b3524ede5a1747a165a9c1
Bill Rastello <github@billrastello.com>
2016-01-08 14:21:49 -0600

    Make marquee work for way more (if not all) images

--- lib/teaas/marquee.rb
+++ lib/teaas/marquee.rb
@@ -1,7 +1,7 @@
 module Teaas
   class Marquee
 
-    # Takes in an image, rolls it 25%, 50%, 75%, and 100%, then returns an animated marquee image. Best when used with {Teaas::Turboize.turbo} to generate multiple marquee speeds.
+    # Takes in an image, rolls it 20%, 40%, 60%, and 80%, then returns an animated marquee image. Best when used with {Teaas::Turboize.turbo} to generate multiple marquee speeds.
     #
     # @param original_img [Magick::ImageList] The image to be created into a marquee
     # @return [Magick::ImageList] The marquee image
@@ -10,16 +10,18 @@ module Teaas
       img = original_img.flatten_images
       img.format = "gif"
 
+      img_width = img.columns
+
       marquee_image << img
-      marquee_image << img.roll(25, 0)
-      marquee_image << img.roll(50, 0)
-      marquee_image << img.roll(75, 0)
-      marquee_image << img.roll(100, 0)
+      marquee_image << img.roll(img_width * 0.2, 0)
+      marquee_image << img.roll(img_width * 0.4, 0)
+      marquee_image << img.roll(img_width * 0.6, 0)
+      marquee_image << img.roll(img_width * 0.8, 0)
 
       marquee_image
     end
 
-    # Takes in an image, rolls it 25%, 50%, 75%, and 100%, then returns an animated marquee image. Best when used with {Teaas::Turboize.turbo} to generate multiple marquee speeds. This is a wrapper around {Teaas::Marquee.marquee}
+    # Takes in an image, rolls it 20%, 40%, 60%, and 80%, then returns an animated marquee image. Best when used with {Teaas::Turboize.turbo} to generate multiple marquee speeds. This is a wrapper around {Teaas::Marquee.marquee}
     #
     # @param path [String] Path to the image to be created to a marquee image
     # @return [Magick::ImageList] The marquee image

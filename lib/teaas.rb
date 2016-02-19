a7259d5165e51101b4948ecef17106cd2073413d
Bill Rastello <github@billrastello.com>
2016-02-19 11:40:54 -0600

    Initial version of intensify

--- lib/teaas.rb
+++ lib/teaas.rb
@@ -1,6 +1,7 @@
 require 'rmagick'
 
 require 'teaas/helper.rb'
+require 'teaas/intensify.rb'
 require 'teaas/marquee.rb'
 require 'teaas/spin.rb'
 require 'teaas/turboize.rb'
---
+++ lib/teaas/intensify.rb
@@ -0,0 +1,46 @@
+module Teaas
+  class Intensify
+
+    GRAVITY = [
+      Magick::NorthWestGravity,
+      Magick::SouthEastGravity,
+      Magick::SouthWestGravity,
+      Magick::NorthEastGravity,
+      Magick::SouthEastGravity,
+      Magick::NorthWestGravity,
+      Magick::SouthWestGravity,
+    ]
+
+    # Takes in an image, composites a smaller version of it, then returns an animated intensified image image. Best when used with {Teaas::Turboize.turbo} to generate multiple intense speeds.
+    #
+    # @param original_img [Array] An array of [Magick::ImageList]s
+    # @return [Magick::ImageList] The intensified image
+    def self.intensify(original_img)
+      intensifty_image = Magick::ImageList.new
+      img = Teaas::Helper.prepare_for_animation(original_img)
+      final_img = Magick::Image.new(img.columns, img.rows)
+      final_img.format = "gif"
+      intensify_image = Magick::ImageList.new
+
+      img.change_geometry("95%x95%") do |cols, rows, i|
+        i.resize!(cols, rows)
+      end
+
+      GRAVITY.each do |g|
+        intensify_image << final_img.composite(img, g, Magick::OverCompositeOp)
+      end
+
+      intensify_image
+    end
+
+    # Takes in an image from a file, composites a smaller version of it, then returns an animated intensified image image. Best when used with {Teaas::Turboize.turbo} to generate multiple intense speeds. Wrapper around {Teaas::Intensifty.intensify}
+    #
+    # @param path [String] Path to the image to be created to an intensified image
+    # @return [Magick::ImageList] The intensified image
+    def self.intensify_from_file(path)
+      img = Magick::Image.read(path)
+
+      intensify(img)
+    end
+  end
+end

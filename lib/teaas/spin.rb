af2b2652b3be465f0cf84af02353ea3b4927ba81
Bill Rastello <github@billrastello.com>
2016-04-01 16:26:46 -0500

    Add ability to spin animated GIFs

--- lib/teaas/spin.rb
+++ lib/teaas/spin.rb
@@ -8,17 +8,12 @@ module Teaas
     def self.spin(original_img, options={})
       rotations = options[:rotations] ? options[:rotations] : 4
       counterclockwise = options[:counterclockwise]
-      spinny_image = Magick::ImageList.new
-      img = Teaas::Helper.prepare_for_animation(original_img)
 
-      increment = 360 / rotations
-
-      rotations.times do |i|
-        temp_img = img.rotate(_increment(increment, i, counterclockwise, rotations)).crop(Magick::NorthWestGravity, original_img.columns, original_img.rows, true)
-        spinny_image << temp_img
+      if options[:animate]
+        _spin_animated_image(original_img, rotations, counterclockwise)
+      else
+        _spin_static_image(original_img, rotations, counterclockwise)
       end
-
-      spinny_image
     end
 
     # Takes in a path to an image, rotates it based on the number of rotations specified, then returns an animated spinning image. Best when used with {Teaas::Turboize.turbo} to generate multiple rotation speeds. This is a wrapper around {Teaas::Spin.spin}
@@ -41,6 +36,37 @@ module Teaas
       else
         increment * (i+1)
       end
+    end
+
+    def self._spin_static_image(original_img, rotations, counterclockwise)
+      spinny_image = Magick::ImageList.new
+      img = Teaas::Helper.prepare_for_animation(original_img)
+
+      increment = 360 / rotations
+
+      rotations.times do |i|
+        temp_img = img.rotate(_increment(increment, i, counterclockwise, rotations)).crop(Magick::NorthWestGravity, original_img.columns, original_img.rows, true)
+        spinny_image << temp_img
+      end
+
+      spinny_image
+    end
+
+    def self._spin_animated_image(original_img, rotations, counterclockwise)
+      frames = original_img.length
+      spinny_image = Magick::ImageList.new
+
+      increment = 360 / rotations
+
+      rotations.times do |i|
+        original_img.each do |img|
+          img.dispose = Magick::BackgroundDispose
+          temp_img = img.rotate(_increment(increment, i, counterclockwise, rotations)).crop(Magick::NorthWestGravity, original_img.columns, original_img.rows, true)
+          spinny_image << temp_img
+        end
+      end
+
+      spinny_image
     end
   end
 end

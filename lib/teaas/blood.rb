ec47519159a04415c67b6ffc31e4fc08e06a4b04
Bill Rastello <github@billrastello.com>
2016-05-13 10:38:13 -0500

    Switch bloodify to use the new overlayer class

--- lib/teaas/blood.rb
+++ lib/teaas/blood.rb
@@ -1,22 +1,11 @@
 module Teaas
   class Blood
-
     # Takes in an image, and adds blood to it. Best when used with {Teaas::Turboize.turbo} to generate multiple rotation speeds.
     #
     # @param original_img [Magick::ImageList] The image to be rotated
     # @return [Magick::ImageList] The spinning image
     def self.blood(original_img)
-      blood_image = Magick::ImageList.new
-      img = Teaas::Helper.prepare_for_animation(original_img)
-      blood_base = Magick::ImageList.new(Teaas.root + "/img/blood.gif")
-      blood_base.each do |image|
-        image.resize_to_fit!(img.columns, img.rows)
-      end
-      blood_image << img
-
-      blood_image = blood_image.composite_layers(blood_base)
-
-      blood_image
+      Overlayer.overlay(original_img, Magick::ImageList.new(Teaas.root + "/img/blood.gif"))
     end
 
     # Takes in a path to an image, and adds blood to it. Best when used with {Teaas::Turboize.turbo} to generate multiple rotation speeds. This is a wrapper around {Teaas::Spin.spin}

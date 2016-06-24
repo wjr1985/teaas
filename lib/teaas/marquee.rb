85221dad36a50dd7e4322257c4406fffc45e0dc5
Bill Rastello <github@billrastello.com>
2016-06-24 13:09:13 -0500

    Remove un-necessary method from marquee

--- lib/teaas/marquee.rb
+++ lib/teaas/marquee.rb
@@ -74,13 +74,5 @@ module Teaas
         Teaas::VerticalRoller
       end
     end
-
-    def self._roll(img, options)
-      if options[:reverse]
-        img.roll(options[:img_width] * ((options[:total_frames] - options[:frame].to_f) / options[:total_frames]), 0)
-      else
-        img.roll(options[:img_width] * (options[:frame].to_f / options[:total_frames]), 0)
-      end
-    end
   end
 end

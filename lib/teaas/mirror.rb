e7727c52ce288633c82aa48b7d94e4f31479a146
Bill Rastello <github@billrastello.com>
2016-07-08 11:17:24 -0500

    Fix formatting in file

--- lib/teaas/mirror.rb
+++ lib/teaas/mirror.rb
@@ -1,6 +1,5 @@
 module Teaas
   class Mirror
-
     def self.mirror(original_img, options = {})
       if original_img[0].format == "GIF" && original_img.length > 1
         mirrored_image = Magick::ImageList.new

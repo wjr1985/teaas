41847a5228880069268968ac4bcdae9b7fa0f042
Bill Rastello <github@billrastello.com>
2017-03-03 09:55:51 -0600

    Add shaking fist overlay

---
+++ lib/teaas/shake_fist.rb
@@ -0,0 +1,25 @@
+module Teaas
+  class ShakeFist
+    # Takes in an image, and adds shake_fist to it. Best when used with {Teaas::Turboize.turbo} to generate multiple rotation speeds.
+    #
+    # @param original_img [Magick::ImageList]
+    # @return [Magick::ImageList]
+    def self.shake_fist(original_img)
+      Overlayer.overlay(original_img, Magick::ImageList.new(Teaas.root + "/img/shake_fist.gif"))
+    end
+
+    # Takes in a path to an image, and adds shake_fist to it. Best when used with {Teaas::Turboize.turbo} to generate multiple rotation speeds. This is a wrapper around {Teaas::Spin.spin}
+    #
+    # @param path [String]
+    # @return [Magick::ImageList]
+    def self.shake_fist_from_file(path)
+      img = Magick::ImageList.new
+
+      # Grab the first element in array to prevent strange things when an
+      # animated image is submitted
+      img.read(path)[0]
+
+      shake_fist(img)
+    end
+  end
+end

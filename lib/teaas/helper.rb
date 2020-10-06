1d6155eca3b1b58b3d3865880eed20e554a9a80b
Bill Rastello <wjr1985@gmail.com>
2020-10-06 08:20:13 -0500

    add ability to force transparency in the helper

--- lib/teaas/helper.rb
+++ lib/teaas/helper.rb
@@ -1,10 +1,10 @@
 module Teaas
   class Helper
-    def self.prepare_for_animation(img)
+    def self.prepare_for_animation(img, options={})
       new_img = img[0]
       new_img.dispose = Magick::BackgroundDispose
       new_img.format = "gif"
-      new_img.background_color = "none"
+      new_img.background_color = options[:force_transparency] ? "transparent" : "none"
 
       new_img
     end

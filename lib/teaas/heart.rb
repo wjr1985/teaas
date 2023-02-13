6d2ccf898f5025c59878e6581acff370e9cf362e
Bill Rastello <wjr1985@gmail.com>
2023-02-13 11:48:02 -0600

    add hearts

---
+++ lib/teaas/heart.rb
@@ -0,0 +1,25 @@
+module Teaas
+  class Heart
+    # Takes in an image, and adds hearts to it. Best when used with {Teaas::Turboize.turbo} to generate multiple rotation speeds.
+    #
+    # @param original_img [Magick::ImageList] The image to be engufled in flames
+    # @return [Magick::ImageList] The image, now on fire
+    def self.heart(original_img, options={})
+      Overlayer.overlay(original_img, Magick::ImageList.new(Teaas.root + "/img/heart.gif"))
+    end
+
+    # Takes in a path to an image, and adds hearts to it. Best when used with {Teaas::Turboize.turbo} to generate multiple rotation speeds. This is a wrapper around {Teaas::Heart.heart}
+    #
+    # @param path [String] Path to the image to be engulfed in flames
+    # @return [Magick::ImageList] The image, now on fire
+    def self.fire_from_file(path, options={})
+      img = Magick::ImageList.new
+
+      # Grab the first element in array to prevent strange things when an
+      # animated image is submitted
+      img.read(path)[0]
+
+      heart(img)
+    end
+  end
+end
